fd45642dd352705aabf7293012e1dff227eca62a
Steffen <99438572+SniperWCW@users.noreply.github.com>
2025-09-14 18:29:39 +0200

    Update __init__.py

--- custom_components/speisekammer/__init__.py
+++ custom_components/speisekammer/__init__.py
@@ -1,8 +1,21 @@
 """Speisekammer Integration"""
-from homeassistant.core import HomeAssistant
 
-DOMAIN = "speisekammer"
+from homeassistant.core import HomeAssistant
+from homeassistant.config_entries import ConfigEntry
+from .const import DOMAIN
+from .sensor import async_setup_entry as sensors_async_setup_entry
 
 async def async_setup(hass: HomeAssistant, config: dict):
-    """Setup der Integration über ConfigFlow (kein YAML)."""
+    """Setup der Integration (ConfigFlow, kein YAML)."""
+    return True
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Setup eines ConfigEntry (wird von Home Assistant aufgerufen)."""
+    # Sensoren registrieren
+    await sensors_async_setup_entry(hass, entry)
+    return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Unload eines ConfigEntry."""
+    # Sensoren werden automatisch über HA entfernt
     return True
