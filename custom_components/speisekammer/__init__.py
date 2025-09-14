7ce51a6b168174260cf729ec52e822cb79ff7afe
Steffen <99438572+SniperWCW@users.noreply.github.com>
2025-09-14 19:34:12 +0200

    __init__.py aktualisieren

--- custom_components/speisekammer/__init__.py
+++ custom_components/speisekammer/__init__.py
@@ -4,12 +4,14 @@ from homeassistant.core import HomeAssistant
 from .const import DOMAIN
 
 async def async_setup(hass: HomeAssistant, config: dict):
-    """Setup ohne ConfigEntry (optional, falls yaml genutzt wird)."""
+    """Setup ohne ConfigEntry (nur falls YAML genutzt wird)."""
     return True
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Setup über ConfigEntry."""
-
-    # Sensor-Plattform laden (korrekte neue Methode)
     await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
     return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Entfernen einer ConfigEntry."""
+    return await hass.config_entries.async_unload_platforms(entry, ["sensor"])
