c2c1aa08c7745737051cfefc9201d381339c50f7
Steffen <99438572+SniperWCW@users.noreply.github.com>
2025-09-14 20:41:55 +0200

    Update sensor.py

--- custom_components/speisekammer/sensor.py
+++ custom_components/speisekammer/sensor.py
@@ -1,24 +1,28 @@
 """Speisekammer Sensoren"""
 
 from datetime import datetime, timedelta
+import logging
+
 from homeassistant.helpers.entity import Entity
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+
 from .api import SpeisekammerAPI
 from .const import CONF_COMMUNITY_ID
-import logging
 
 _LOGGER = logging.getLogger(__name__)
 
-async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
-    """Setup Sensoren über Platform (wird von ConfigEntry aufgerufen)."""
-    token = config.get("token")
-    community_id = config.get(CONF_COMMUNITY_ID)
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_entities):
+    """Setup Sensoren für ConfigEntry."""
+    token = entry.data.get("token")
+    community_id = entry.data.get(CONF_COMMUNITY_ID)
     api = SpeisekammerAPI(token)
 
     sensors = [
         ExpiringItemsSensor(api, community_id),
         TotalItemsSensor(api, community_id),
         ItemsPerLocationSensor(api, community_id),
-        ItemsPerLocationDetailsSensor(api, community_id)
+        ItemsPerLocationDetailsSensor(api, community_id),
     ]
 
     async_add_entities(sensors, update_before_add=True)
@@ -151,7 +155,7 @@ class ItemsPerLocationSensor(Entity):
 
 
 class ItemsPerLocationDetailsSensor(Entity):
-    """Sensor: Artikel pro Lagerort mit Details."""
+    """Sensor: Detaillierte Auflistung pro Lagerort (für Markdown-Karte)."""
 
     def __init__(self, api: SpeisekammerAPI, community_id: str):
         self._api = api
@@ -161,11 +165,11 @@ class ItemsPerLocationDetailsSensor(Entity):
 
     @property
     def name(self):
-        return "Speisekammer Artikel pro Lagerort Details"
+        return "Speisekammer Artikel Details je Lagerort"
 
     @property
     def state(self):
-        return sum([len(v) for v in self._attributes.values()]) if self._attributes else 0
+        return self._state
 
     @property
     def extra_state_attributes(self):
@@ -173,26 +177,37 @@ class ItemsPerLocationDetailsSensor(Entity):
 
     async def async_update(self):
         try:
-            # Alle Lagerorte abrufen
-            storage_locations = await self._api.get_storage_locations(self._community_id)
+            # Alle Lagerorte abfragen
+            locations = await self._api.get_storage_locations(self._community_id)
             details = {}
-            for loc in storage_locations:
-                loc_id = loc["id"]
-                loc_name = loc.get("name", loc_id)
+            total_items = 0
+
+            for loc in locations:
+                loc_id = loc.get("id")
+                loc_name = loc.get("name", "Unbekannt")
                 items = await self._api.get_stock(self._community_id, loc_id)
-                details[loc_name] = [
-                    {
-                        "name": i["name"],
-                        "gtin": i["gtin"],
-                        "count": a["count"],
-                        "bestBeforeDate": a.get("bestBeforeDate")
-                    }
-                    for i in items
-                    for a in i.get("attributes", [])
-                ]
+                item_list = []
+
+                for item in items:
+                    name = item.get("name")
+                    gtin = item.get("gtin")
+                    for attr in item.get("attributes", []):
+                        count = attr.get("count")
+                        expiry = attr.get("bestBeforeDate")
+                        item_list.append({
+                            "name": name,
+                            "gtin": gtin,
+                            "count": count,
+                            "expires": expiry
+                        })
+                        total_items += count
+
+                details[loc_name] = item_list
+
             self._attributes = details
-            self._state = sum([len(v) for v in details.values()])
+            self._state = total_items
+
         except Exception as e:
-            _LOGGER.error("Fehler beim Abrufen der Artikel pro Lagerort Details: %s", e)
+            _LOGGER.error("Fehler beim Abrufen der Lagerort-Details: %s", e)
             self._state = 0
             self._attributes = {}
