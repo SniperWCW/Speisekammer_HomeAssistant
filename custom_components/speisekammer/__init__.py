6d5b5d800cab8e874de0bb6fb77f7a2f8369f2e3
Steffen <99438572+SniperWCW@users.noreply.github.com>
2025-09-14 18:24:59 +0200

    Update __init__.py

--- custom_components/speisekammer/__init__.py
+++ custom_components/speisekammer/__init__.py
@@ -1,2 +1,8 @@
-"""Speisekammer Integration."""
-# leer, keine weiteren imports nötig
+"""Speisekammer Integration"""
+from homeassistant.core import HomeAssistant
+
+DOMAIN = "speisekammer"
+
+async def async_setup(hass: HomeAssistant, config: dict):
+    """Setup der Integration über ConfigFlow (kein YAML)."""
+    return True
