e5b5fb2db5336a8ae348a6dfc9c5637de10131c1
Steffen <99438572+SniperWCW@users.noreply.github.com>
2025-09-14 20:36:27 +0200

    Update sensor.py

--- custom_components/speisekammer/sensor.py
+++ custom_components/speisekammer/sensor.py
@@ -1,3 +1,5 @@
+"""Speisekammer Sensoren"""
+
 from datetime import datetime, timedelta
 from homeassistant.helpers.entity import Entity
 from .api import SpeisekammerAPI
@@ -6,22 +8,22 @@ import logging
 
 _LOGGER = logging.getLogger(__name__)
 
-async def async_setup_entry(hass, entry, async_add_entities):
-    """Setup Sensoren über ConfigEntry."""
-    token = entry.data.get("token")
-    community_id = entry.data.get(CONF_COMMUNITY_ID)
+async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
+    """Setup Sensoren über Platform (wird von ConfigEntry aufgerufen)."""
+    token = config.get("token")
+    community_id = config.get(CONF_COMMUNITY_ID)
     api = SpeisekammerAPI(token)
 
     sensors = [
         ExpiringItemsSensor(api, community_id),
         TotalItemsSensor(api, community_id),
-        ItemsPerLocationSensor(api, community_id)
+        ItemsPerLocationSensor(api, community_id),
+        ItemsPerLocationDetailsSensor(api, community_id)
     ]
 
     async_add_entities(sensors, update_before_add=True)
 
 
-# ----- Sensoren -----
 class ExpiringItemsSensor(Entity):
     """Sensor: Anzahl Artikel mit MHD in den nächsten X Tagen."""
 
@@ -63,11 +65,11 @@ class ExpiringItemsSensor(Entity):
         expiring = []
 
         for item in items:
-            exp_date_str = item.get("bestBeforeDate") or item.get("expiryDate")
+            exp_date_str = item.get("expirationDate") or item.get("expiryDate")
             if not exp_date_str:
                 continue
             try:
-                exp_date = datetime.fromisoformat(exp_date_str.replace("Z", "+00:00"))
+                exp_date = datetime.fromisoformat(exp_date_str)
             except ValueError:
                 continue
             if now <= exp_date <= cutoff:
@@ -75,7 +77,7 @@ class ExpiringItemsSensor(Entity):
 
         self._state = len(expiring)
         self._attributes = {
-            "items": [{"name": i.get("name"), "expires": i.get("bestBeforeDate")} for i in expiring]
+            "items": [{"name": i.get("name"), "expires": i.get("expirationDate")} for i in expiring]
         }
 
 
@@ -146,3 +148,51 @@ class ItemsPerLocationSensor(Entity):
 
         self._attributes = locations
         self._state = sum(locations.values())
+
+
+class ItemsPerLocationDetailsSensor(Entity):
+    """Sensor: Artikel pro Lagerort mit Details."""
+
+    def __init__(self, api: SpeisekammerAPI, community_id: str):
+        self._api = api
+        self._community_id = community_id
+        self._state = None
+        self._attributes = {}
+
+    @property
+    def name(self):
+        return "Speisekammer Artikel pro Lagerort Details"
+
+    @property
+    def state(self):
+        return sum([len(v) for v in self._attributes.values()]) if self._attributes else 0
+
+    @property
+    def extra_state_attributes(self):
+        return self._attributes
+
+    async def async_update(self):
+        try:
+            # Alle Lagerorte abrufen
+            storage_locations = await self._api.get_storage_locations(self._community_id)
+            details = {}
+            for loc in storage_locations:
+                loc_id = loc["id"]
+                loc_name = loc.get("name", loc_id)
+                items = await self._api.get_stock(self._community_id, loc_id)
+                details[loc_name] = [
+                    {
+                        "name": i["name"],
+                        "gtin": i["gtin"],
+                        "count": a["count"],
+                        "bestBeforeDate": a.get("bestBeforeDate")
+                    }
+                    for i in items
+                    for a in i.get("attributes", [])
+                ]
+            self._attributes = details
+            self._state = sum([len(v) for v in details.values()])
+        except Exception as e:
+            _LOGGER.error("Fehler beim Abrufen der Artikel pro Lagerort Details: %s", e)
+            self._state = 0
+            self._attributes = {}
