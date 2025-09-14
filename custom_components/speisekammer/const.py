7f85d581b534c08cc6f580f2a28e51ab467c0e2d
Steffen <99438572+SniperWCW@users.noreply.github.com>
2025-09-14 19:57:58 +0200

    Update const.py

--- custom_components/speisekammer/const.py
+++ custom_components/speisekammer/const.py
@@ -1,5 +1,10 @@
-"""Konstanten für Speisekammer Integration"""
+"""Konstanten für Speisekammer Integration."""
 
 DOMAIN = "speisekammer"
+
+# Config Keys
 CONF_TOKEN = "token"
 CONF_COMMUNITY_ID = "community_id"
+
+# Versionsinfo (hilfreich für Debugging)
+INTEGRATION_VERSION = "1.0.0"
