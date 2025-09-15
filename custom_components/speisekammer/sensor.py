6ea3545be2f87ec005300df4857e047efa86041f
Steffen <99438572+SniperWCW@users.noreply.github.com>
2025-09-15 13:55:11 +0200

    Update sensor.py

--- custom_components/speisekammer/sensor.py
+++ custom_components/speisekammer/sensor.py
@@ -1,48 +1,69 @@
-from homeassistant.helpers.entity import SensorEntity
-from .const import DOMAIN
+from datetime import timedelta
+from homeassistant.components.sensor import SensorEntity
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
 from .api import SpeisekammerAPI
+from .const import DOMAIN, CONF_TOKEN, CONF_COMMUNITY_ID
 
-async def async_setup_entry(hass, entry, async_add_entities):
-    token = entry.data["token"]
-    community_id = entry.data["community_id"]
-    api = SpeisekammerAPI(token)
+# ⏱️ Sensor wird alle 10 Minuten aktualisiert
+SCAN_INTERVAL = timedelta(minutes=10)
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_entities):
+    token = entry.data[CONF_TOKEN]
+    community_id = entry.data[CONF_COMMUNITY_ID]
+    api = SpeisekammerAPI(token=token)
+
     locations = await api.get_storage_locations(community_id)
+    entities = []
 
-    sensors = []
     for location in locations:
-        sensors.append(StorageLocationSensor(location))
-    async_add_entities(sensors)
+        location_id = location["id"]
+        location_name = location["name"]
+        entities.append(StorageLocationSensor(api, community_id, location_id, location_name))
+
+    async_add_entities(entities, update_before_add=True)
 
 class StorageLocationSensor(SensorEntity):
-    def __init__(self, location):
-        self._location = location
-        self._name = f"Lagerplatz: {location['name']}"
-        self._state = None
-        self._attributes = {}
+    """Sensor für einen Lagerort mit flex-table-kompatiblen Attributen."""
+
+    def __init__(self, api: SpeisekammerAPI, community_id: str, location_id: str, location_name: str):
+        self._api = api
+        self._community_id = community_id
+        self._location_id = location_id
+        self._location_name = location_name
+
+        self._attr_name = f"Lagerplatz: {location_name}"
+        self._attr_unique_id = f"speisekammer_{location_id}"
+        self._attr_icon = "mdi:package-variant"
+        self._attr_unit_of_measurement = "Artikel"
+        self._attr_state = None
+        self._attr_extra_state_attributes = {}
 
     async def async_update(self):
-        items = self._location.get("items", [])
-        self._state = sum(item["amount"] for item in items)
-        self._attributes = {
-            "table": [
-                {
-                    "Name": item["name"],
-                    "Menge": item["amount"],
-                    "GTIN": item.get("gtin", ""),
-                    "Ablaufdatum": item.get("expirationDate", "")
-                }
-                for item in items
-            ]
+        items = await self._api.get_items(self._community_id, self._location_id)
+        if not items:
+            self._attr_state = 0
+            self._attr_extra_state_attributes = {
+                "table": [],
+                "Lagerplatz": self._location_name,
+                "Artikelanzahl": 0
+            }
+            return
+
+        table = []
+        for item in items:
+            for attr in item.get("attributes", []):
+                if attr.get("count", 0) > 0:
+                    table.append({
+                        "Name": item.get("name", "Unbekannt"),
+                        "Menge": attr.get("count", 1),
+                        "GTIN": item.get("gtin", ""),
+                        "Ablaufdatum": attr.get("bestBeforeDate", "")
+                    })
+
+        self._attr_state = len(table)
+        self._attr_extra_state_attributes = {
+            "table": table,
+            "Lagerplatz": self._location_name,
+            "Artikelanzahl": len(table)
         }
-
-    @property
-    def name(self):
-        return self._name
-
-    @property
-    def state(self):
-        return self._state
-
-    @property
-    def extra_state_attributes(self):
-        return self._attributes
